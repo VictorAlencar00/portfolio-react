70a95ba360ca0c417259a1f8e0026189a1eed8fa
VictorAlencar00 <victordesousaa@gmail.com>
2024-04-14 18:22:38 -0300

    feat: curriculum download add

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -41,15 +41,26 @@ export default function Home() {
           </h3>
         </div>
         <div className="flex gap-2 justify-center sm:justify-normal">
-          <button className="bg-zinc-950 text-white h-12 p-2 rounded-3xl border border-solid hover:scale-110 transition-all hidden sm:block hover:bg-softWhite hover:text-black">
+          <a
+            href="https://drive.google.com/file/d/1Bbcm1iDH6_xd99Wb9YDahf5La6Ih1do7/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-zinc-950 text-white h-12 p-2 rounded-3xl border border-solid hover:scale-110 transition-all hidden hover:bg-softWhite hover:text-black sm:flex items-center"
+          >
             Baixar Currículo
-          </button>
-          <button className="bg-zinc-950 text-white h-12 p-2 rounded-3xl border border-solid hover:scale-110 transition-all sm:hidden hover:bg-softWhite hover:text-black">
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1Bbcm1iDH6_xd99Wb9YDahf5La6Ih1do7/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-zinc-950 text-white h-12 p-2 rounded-3xl border border-solid hover:scale-110 transition-all sm:hidden hover:bg-softWhite hover:text-black flex items-center"
+          >
             Currículo
-          </button>
+          </a>
           <a
             href="#contacts"
-            className="bg-zinc-950 text-white h-12 p-2 rounded-3xl border border-solid hover:scale-110 transition-all hover:bg-softWhite hover:text-black"
+            className="bg-zinc-950 text-white h-12 p-2 rounded-3xl border border-solid hover:scale-110 transition-all hover:bg-softWhite hover:text-black
+            flex items-center"
           >
             Contato
           </a>
